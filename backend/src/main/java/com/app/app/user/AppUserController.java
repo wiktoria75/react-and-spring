b9bed3c6ce4bb911274ee2ed6b94a0c2b17a7033
Wiktoria Kulczynska <wiktoriakulczynska@gmail.com>
2024-02-09 18:33:51 +0100

    As a user I want to register so I can create new account #9 / Join front with backend

--- backend/src/main/java/com/app/app/user/AppUserController.java
+++ backend/src/main/java/com/app/app/user/AppUserController.java
@@ -3,15 +3,16 @@ package com.app.app.user;
 import com.app.app.user.model.RegisteredUserDto;
 import com.app.app.user.service.AppUserService;
 import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Valid;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000/register")
 public class AppUserController {
 
     private final AppUserService appUserService;
@@ -21,7 +22,7 @@ public class AppUserController {
     }
 
     @PostMapping(value = "/register")
-    public ResponseEntity<Object> addUser(@Valid @RequestBody RegisteredUserDto userDTO) {
+    public ResponseEntity<Object> addUser(@RequestBody RegisteredUserDto userDTO) {
         try {
             appUserService.registerUser(userDTO);
             return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
